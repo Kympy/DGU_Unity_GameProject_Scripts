656e1c0b87bfca8e15a1f84b2f0c4891b4e5ae30
Kympy <gftr9905@gmail.com>
2020-05-17 21:21:42 +0900

    Create Portal

---
+++ PortalEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalEnter : MonoBehaviour
+{
+    public GameObject player;
+
+    bool isPlayerAtPortal = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            isPlayerAtPortal = true;
+        }
+    }
+    private void Update()
+    {
+        if(isPlayerAtPortal == true)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
